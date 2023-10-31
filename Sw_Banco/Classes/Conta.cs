239fd27d6143e0327b73d374c7bec3f352bffcb7
Luizsellitto <luizsellitto2609@gmail.com>
2023-10-31 14:06:22 -0400

    Inicio

---
+++ Sw_Banco/Classes/Conta.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sw_Banco.Classes
+{
+    internal class Conta
+    {
+        public int Num { get; set; }
+        public string Agencia { get; set; }
+        public string Titular { get; set; }
+        public double Saldo { get; set; }
+
+        public Conta(int num, string agencia, string titular, double saldo)
+        {
+            Num = num;
+            Agencia = agencia;
+            Titular = titular;
+            Saldo = saldo;
+        }
+
+        public virtual void Sacar(double valor)
+        {
+            if ((Saldo - valor) <= 0)
+            {
+                Console.WriteLine("Saldo insulficiente para o saque");
+            }
+            else
+            {
+                Saldo = Saldo - valor;
+                Console.WriteLine($"O valor de R${valor} foi sacado com sucesso");
+            }
+        }
+        public void Depositar(double valor)
+        {
+            Saldo += valor;
+        }
+
+    }
+}
